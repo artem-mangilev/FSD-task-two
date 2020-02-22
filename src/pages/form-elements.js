319d1d79a305c9a0dadcc78a9533ce9e7b4aaa3f
Artem <artmnglv@gmail.com>
2020-02-22 20:39:19 +0500

    add new text field & dropdown examples and remove the old ones

--- src/pages/form-elements.js
+++ src/pages/form-elements.js
@@ -1,6 +1,8 @@
 import '../layout/base'
 
 import '../components/field/field'
+import '../components/input/input'
+import '../components/dropdown/dropdown'
 
 import '../variables.scss'
 import '../style.scss'
