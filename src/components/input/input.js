edcd292cd4b3388a4e6a48209497973435300c32
Artem <artmnglv@gmail.com>
2020-02-23 16:17:49 +0500

    add jquery & inputmask plugin, implement date mask in input component

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,1 +1,15 @@
 import './input.scss'
+
+import $ from 'jquery'
+import 'inputmask/dist/jquery.inputmask'
+
+$(document).ready(() => {
+  const maskParameters = { 
+    alias: 'datetime', 
+    inputFormat: 'dd.mm.yyyy',
+    placeholder: 'ДД.ММ.ГГГГ' 
+  }
+
+  $('.input__field_masked').inputmask(maskParameters)  
+})
+
