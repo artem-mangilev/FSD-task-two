49c6751cf1cf5c2853cd74d825644d2d4a3474e5
Artem <artmnglv@gmail.com>
2020-02-24 14:51:38 +0500

    implement checkbox component

--- src/pages/form-elements.js
+++ src/pages/form-elements.js
@@ -3,6 +3,7 @@ import '../layout/base'
 import '../components/field/field'
 import '../components/input/input'
 import '../components/dropdown/dropdown'
+import '../components/checkbox/checkbox'
 
 import '../variables.scss'
 import '../style.scss'
