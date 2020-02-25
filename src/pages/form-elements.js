1513b0341386294aabbc6b0ba1725a9974221859
Artem <artmnglv@gmail.com>
2020-02-25 13:28:30 +0500

    add radio example

--- src/pages/form-elements.js
+++ src/pages/form-elements.js
@@ -4,6 +4,7 @@ import '../components/field/field'
 import '../components/input/input'
 import '../components/dropdown/dropdown'
 import '../components/checkbox-group/checkbox-group'
+import '../components/radio/radio'
 
 import '../variables.scss'
 import '../style.scss'
