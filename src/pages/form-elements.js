ea0216c7cbee504c2bba5bfec88190f4b5d6b97b
Artem <artmnglv@gmail.com>
2020-02-26 17:25:57 +0500

    implement toggle component

--- src/pages/form-elements.js
+++ src/pages/form-elements.js
@@ -5,6 +5,7 @@ import '../components/input/input'
 import '../components/dropdown/dropdown'
 import '../components/checkbox-group/checkbox-group'
 import '../components/radio-group/radio-group'
+import '../components/toggle/toggle'
 
 import '../variables.scss'
 import '../style.scss'
