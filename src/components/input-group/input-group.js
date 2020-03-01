9cd8b09d9a2c5506261f7e9a63bc48e851fc2c62
Artem <artmnglv@gmail.com>
2020-03-01 20:42:26 +0500

    add common component for grouping input elements

---
+++ src/components/input-group/input-group.js
@@ -0,0 +1,8 @@
+import './input-group.scss'
+
+import '../checkbox/checkbox'
+import '../radio/radio'
+import '../toggle/toggle'
+import '../like-button/like-button'
+import '../rate-button/rate-button'
+
