b8a7442d2232acb4a133c097d5532fc6f39db9e6
Artem <artmnglv@gmail.com>
2020-03-04 21:59:33 +0500

    implement item quantity dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,1 +1,28 @@
 import './dropdown.scss'
+
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js'
+// import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css'
+import plural from 'plural-ru'
+
+$(document).ready(() => {
+  const $dropdown = $('.iqdropdown')
+
+  if ($dropdown.data('type') === 'item-quantity') {
+    const items = $dropdown.data('items')
+
+    $dropdown.iqDropdown({
+      setSelectionText: itemCount => {
+        const pluralizedItems = items.map((item, index) => {
+          return `${itemCount[index]} ${plural(
+            itemCount[index],
+            ...item.nameForms
+          )}`
+        })
+
+        let selectionText = pluralizedItems.slice(0, 2).join(', ') + '...'
+
+        return selectionText
+      }
+    })
+  }
+})
