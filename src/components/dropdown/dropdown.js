8ee6abb1857557ccaae9ed6ad53f021d99318635
Artem <artmnglv@gmail.com>
2020-03-07 17:53:17 +0500

    add possibility to spawn several separated dropdowns

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -15,76 +15,93 @@ $(document).ready(() => {
   const decrementBtnClass = dropdownBlockClass + '__decrement-button'
   const quantityClass = dropdownBlockClass + '__quantity'
 
-  const $dropdown = $(`.${dropdownBlockClass}`)
-  const $selection = $dropdown.find(`.${selectionClass}`)
-  const $selectionText = $selection.find(`.${selectionTextClass}`)
-  const $menu = $dropdown.find(`.${menuElementClass}`)
-  const $options = $menu.find(`.${optionClass}`)
+  const $dropdowns = $(`.${dropdownBlockClass}`)
 
-  const itemsState = $dropdown.data('items')
+  $dropdowns.each((index, dropdown) => {
+    const $dropdown = $(dropdown)
+    const $selection = $dropdown.find(`.${selectionClass}`)
+    const $selectionText = $selection.find(`.${selectionTextClass}`)
+    const $menu = $dropdown.find(`.${menuElementClass}`)
+    const $options = $menu.find(`.${optionClass}`)
 
-  const renderItems = () => {
-    $options.each((index, el) => {
-      const $quantity = $(el).find(`.${quantityClass}`)
-      $quantity.html(`${itemsState[index].quantity}`)
-    })
-  }
+    const itemsState = $dropdown.data('items')
+    const { type } = $dropdown.data('meta')
 
-  const renderSelectionText = () => {
-    const selectionText = itemsState
-      .map(({ quantity, nameForms }) => {
-        return `${quantity} ${plural(quantity, ...nameForms)}`
+    const totalItems = () =>
+      itemsState.reduce((accum, currentItem) => accum + currentItem.quantity, 0)
+
+    const renderItems = () => {
+      $options.each((index, el) => {
+        const $quantity = $(el).find(`.${quantityClass}`)
+        $quantity.html(`${itemsState[index].quantity}`)
       })
-      .slice(0, itemsState.length >= 2 ? 2 : undefined)
-      .join(', ')
-      .concat('...')
+    }
 
-    $selectionText.html(selectionText)
-  }
+    const renderSelectionText = () => {
+      let selectionText
 
-  const renderDecrementButtons = () => {
-    $options.each((index, option) => {
-      $(option)
-        .find(`.${decrementBtnClass}`)
-        .prop('disabled', itemsState[index].quantity === 0)
-    })
-  }
+      if (type === 'item-quantity') {
+        selectionText = itemsState
+          .map(({ quantity, nameForms }) => {
+            return `${quantity} ${plural(quantity, ...nameForms)}`
+          })
+          .slice(0, itemsState.length >= 2 ? 2 : undefined)
+          .join(', ')
+          .concat('...')
+      } else if (type === 'item-quantity-applied') {
+        const { selectionTextForms } = $dropdown.data('meta')
+        const total = totalItems()
 
-  const render = () => {
-    renderItems()
-    renderDecrementButtons()
-    renderSelectionText()
-  }
-
-  const init = () => {
-    renderSelectionText()
-    renderDecrementButtons()
-  }
-
-  init()
-
-  $options.click(function({ target: { classList } }) {
-    const optionId = $(this).data('id')
-
-    const targetItem = itemsState[optionId]
-
-    const isButton = classList.contains(btnClass)
-    if (isButton) {
-      const isIncrementBtn = classList.contains(incrementBtnClass)
-      const isDecrementBtn = classList.contains(decrementBtnClass)
-      if (isIncrementBtn) {
-        targetItem.quantity += 1
-      } else if (isDecrementBtn) {
-        if (targetItem.quantity > 0) targetItem.quantity -= 1
-        else return
+        selectionText = `${total} ${plural(total, ...selectionTextForms)}`
       }
 
-      render()
+      $selectionText.html(selectionText)
     }
-  })
 
-  $selection.click(() => {
-    $menu.toggleClass(menuElementClass + '_hide')
-    $dropdown.toggleClass(dropdownBottonBorderlessModifierClass)
+    const renderDecrementButtons = () => {
+      $options.each((index, option) => {
+        $(option)
+          .find(`.${decrementBtnClass}`)
+          .prop('disabled', itemsState[index].quantity === 0)
+      })
+    }
+
+    const render = () => {
+      renderItems()
+      renderDecrementButtons()
+      renderSelectionText()
+    }
+
+    const init = () => {
+      renderSelectionText()
+      renderDecrementButtons()
+    }
+
+    init()
+
+    $options.click(function({ target: { classList } }) {
+      const optionId = $(this).data('id')
+
+      const targetItem = itemsState[optionId]
+
+      const isButton = classList.contains(btnClass)
+      if (isButton) {
+        const isIncrementBtn = classList.contains(incrementBtnClass)
+        const isDecrementBtn = classList.contains(decrementBtnClass)
+        if (isIncrementBtn) {
+          targetItem.quantity += 1
+        } else if (isDecrementBtn) {
+          if (targetItem.quantity > 0) targetItem.quantity -= 1
+          else return
+        }
+
+        render()
+      }
+    })
+
+    $selection.click(() => {
+      $menu.toggleClass(menuElementClass + '_hide')
+      $dropdown.toggleClass(dropdownBottonBorderlessModifierClass)
+    })
   })
 })
