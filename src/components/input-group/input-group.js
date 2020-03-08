2b7e48081b2c0c89335116c8ef93d3dd3b46a729
Artem <artmnglv@gmail.com>
2020-03-08 20:55:54 +0500

    implement expandable checkbox list

--- src/components/input-group/input-group.js
+++ src/components/input-group/input-group.js
@@ -6,3 +6,16 @@ import '../toggle/toggle'
 import '../like-button/like-button'
 import '../rate-button/rate-button'
 
+$(document).ready(() => {
+  const $expandableCheckboxGroup = $(
+    '.input-group[data-type="checkbox-expandable"]'
+  )
+  const $name = $expandableCheckboxGroup.find('.input-group__name')
+  const $members = $expandableCheckboxGroup.find('.input-group__members')
+  const $expandIcon = $expandableCheckboxGroup.find('.input-group__expand-icon')
+
+  $name.click(() => {
+    $members.toggleClass('input-group__members_hide')
+    $expandIcon.toggleClass('input-group__expand-icon_rotated')
+  })
+})
