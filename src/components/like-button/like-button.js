1f3b5fb04b7f0a5035523fca3cb190b739ed4f09
Artem <artmnglv@gmail.com>
2020-03-10 12:15:21 +0500

    implement interactivity in like component

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,1 +1,26 @@
 import './like-button.scss'
+
+$(document).ready(() => {
+  const $likeButtons = $('.like-button')
+
+  $likeButtons.each((index, button) => {
+    $(button).click(function() {
+      const $this = $(this)
+
+      const $likesNumber = $this.find('.like-button__likes_number')
+
+      $this.toggleClass('like-button_active')
+
+      let likes = $this.data('likes')
+
+      if ($this.hasClass('like-button_active')) {
+        likes++
+      } else {
+        likes--
+      }
+      $this.data('likes', likes)
+
+      $likesNumber.html(likes)
+    })
+  })
+})
