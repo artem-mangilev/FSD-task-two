5bacc65bb7afe0de3b59daad2648823cd6d95e65
Artem <artmnglv@gmail.com>
2020-03-16 16:43:29 +0500

    implement checkbox-list component. It has more intuitive interface than input-group

---
+++ src/components/checkbox-list/checkbox-list.js
@@ -0,0 +1,22 @@
+import './checkbox-list.scss'
+
+$(document).ready(() => {
+  const $checkboxList = $('.checkbox-list_expandable')
+  const $title = $checkboxList.find('.checkbox-list__title')
+  const $checkboxes = $checkboxList.find('.checkbox-list__checkboxes')
+
+  const expandedClass = 'checkbox-list__title_expanded'
+  const hiddenClass = 'checkbox-list__title_hidden'
+
+  $title.click(function() {
+    const expanded = $title.hasClass(expandedClass)
+
+    if (expanded) {
+      $title.removeClass(expandedClass).addClass(hiddenClass)
+      $checkboxes.css('display', 'none')
+    } else {
+      $title.removeClass(hiddenClass).addClass(expandedClass)
+      $checkboxes.css('display', 'block')
+    }
+  })
+})
