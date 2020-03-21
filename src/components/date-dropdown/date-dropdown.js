0f94d911687be197c5f316b7480c715c4d14e9a0
Artem <artmnglv@gmail.com>
2020-03-21 17:47:50 +0500

    execure date dropdown code when document is ready

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -3,43 +3,52 @@ import './date-dropdown.scss'
 import 'air-datepicker/dist/js/datepicker'
 import 'air-datepicker/dist/css/datepicker.css'
 
-const $date = $('.date-dropdown__input')
-const $fromDate = $('.date-dropdown__input_from')
-const $toDate = $('.date-dropdown__input_to')
+$(document).ready(() => {
+  const $date = $('.date-dropdown__input')
+  const $fromDate = $('.date-dropdown__input_from')
+  const $toDate = $('.date-dropdown__input_to')
+  const $rangeFrom = $('-range-from-')
+  const $cells = $('.datepicker--cells')
 
-$fromDate.attr('placeholder', 'ДД.ММ.ГГГГ')
-$toDate.attr('placeholder', 'ДД.ММ.ГГГГ')
+  $fromDate.attr('placeholder', 'ДД.ММ.ГГГГ')
+  $toDate.attr('placeholder', 'ДД.ММ.ГГГГ')
 
-let selected = true
-$date.datepicker({
-  minDate: new Date(),
-  navTitles: {
-    days: 'MM yyyy'
-  },
-  prevHtml: '<i class="material-icons">arrow_back</i>',
-  nextHtml: '<i class="material-icons">arrow_forward</i>',
-  onSelect: (formattedDate, date, { $el }) => {
-    // if range selected
-    if (date.length === 2) {
-      selected = !selected
+  let selected = true
+  $date.datepicker({
+    minDate: new Date(),
+    navTitles: {
+      days: 'MM yyyy'
+    },
+    prevHtml: '<i class="material-icons">arrow_back</i>',
+    nextHtml: '<i class="material-icons">arrow_forward</i>',
+    onSelect: (formattedDate, date, { $el }) => {
+      // if range selected
+      if (date.length === 2) {
+        selected = !selected
 
-      const [fromDate, toDate] = formattedDate.split(',')
+        const [fromDate, toDate] = formattedDate.split(',')
 
-      if ($el.hasClass('date-dropdown__input_from')) {
-        $el.val(fromDate)
-        $toDate.val(toDate)
+        if ($el.hasClass('date-dropdown__input_from')) {
+          $el.val(fromDate)
+          $toDate.val(toDate)
 
-        if (!selected) {
-          $toDate.data('datepicker').selectDate(date)
-        }
-      } else if ($el.hasClass('date-dropdown__input_to')) {
-        $el.val(toDate)
-        $fromDate.val(fromDate)
+          if (!selected) {
+            $toDate.data('datepicker').selectDate(date)
+          }
+        } else if ($el.hasClass('date-dropdown__input_to')) {
+          $el.val(toDate)
+          $fromDate.val(fromDate)
 
-        if (!selected) {
-          $fromDate.data('datepicker').selectDate(date)
+          if (!selected) {
+            $fromDate.data('datepicker').selectDate(date)
+          }
         }
       }
     }
-  }
+  })
+
+  $cells.click(function(e) {
+    const $fromCell = $(e.target)
+    $fromCell.addClass('-range-single-')
+  })
 })
