ebbb1a3121967a012103b716e472a05d5105d4f8
Artem <artmnglv@gmail.com>
2020-03-23 13:03:39 +0500

    desctucture instance object and get rid unnecessary code

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -18,33 +18,35 @@ $(document).ready(() => {
     clearButton: true,
     prevHtml: '<i class="material-icons">arrow_back</i>',
     nextHtml: '<i class="material-icons">arrow_forward</i>',
-    onSelect: function(formattedDate, date, instance) {
+    onSelect: function(
+      formattedDate,
+      date,
+      { $datepicker, $el, opts: { multipleDatesSeparator } }
+    ) {
       // prevent writing selected date in input
-      instance.$el.val('')
+      $el.val('')
 
       // if it's not a range, just skip
       if (date.length !== 2) return
 
       // find apply button in datepicker
-      const $applyButton = instance.$datepicker.find(
+      const $applyButton = $datepicker.find(
         '.datepicker--button[data-action="apply"]'
       )
       // if apply button was clicked
       $applyButton.click(() => {
-        // in range, formattedDate is a string of 2 dates divided
-        // by separator specified in plugin settings
-        const separator = instance.opts.multipleDatesSeparator
+        // in range, formattedDate is a string of 2 dates divided by multileDatesSeparator
         // extract from & to dates from formattedDate
-        const [fromDate, toDate] = formattedDate.split(separator)
+        const [fromDate, toDate] = formattedDate.split(multipleDatesSeparator)
         // set fromDate to actual input
-        instance.$el.val(fromDate)
+        $el.val(fromDate)
         // set toDate to fake input
         $toDate.val(toDate)
       })
 
       // when clear button is pressed, both real and fake inputs have to be cleared.
       // find clear button in current instance of datepicker
-      const $clearButton = instance.$datepicker.find(
+      const $clearButton = $datepicker.find(
         '.datepicker--button[data-action="clear"]'
       )
       // if clear button is pressed
@@ -52,8 +54,6 @@ $(document).ready(() => {
         // clear fake input
         $toDate.val('')
       })
-
-      console.log(instance)
     }
   }
 
