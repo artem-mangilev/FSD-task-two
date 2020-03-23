937c6cadc4acc7f7bdc879566710b81bc6c88848
Artem <artmnglv@gmail.com>
2020-03-23 12:52:52 +0500

    reimplement multiple datepicker logic: the second input now is just stores toDate and activates the first input if clicked

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -4,52 +4,67 @@ import 'air-datepicker/dist/js/datepicker'
 import 'air-datepicker/dist/css/datepicker.css'
 
 $(document).ready(() => {
-  const $date = $('.date-dropdown__input')
   const $fromDate = $('.date-dropdown__input_from')
   const $toDate = $('.date-dropdown__input_to')
-  const $rangeFrom = $('-range-from-')
-  const $cells = $('.datepicker--cells')
 
   $fromDate.attr('placeholder', 'ДД.ММ.ГГГГ')
   $toDate.attr('placeholder', 'ДД.ММ.ГГГГ')
 
-  let selected = true
-  const datepicker = $date
-    .datepicker({
-      minDate: new Date(),
-      navTitles: {
-        days: 'MM yyyy'
-      },
-      clearButton: true,
-      prevHtml: '<i class="material-icons">arrow_back</i>',
-      nextHtml: '<i class="material-icons">arrow_forward</i>',
-      onSelect: (formattedDate, date, { $el }) => {
-        // if range selected
-        if (date.length === 2) {
-          selected = !selected
+  const datepickerSettings = {
+    minDate: new Date(),
+    navTitles: {
+      days: 'MM yyyy'
+    },
+    clearButton: true,
+    prevHtml: '<i class="material-icons">arrow_back</i>',
+    nextHtml: '<i class="material-icons">arrow_forward</i>',
+    onSelect: function(formattedDate, date, instance) {
+      // prevent writing selected date in input
+      instance.$el.val('')
 
-          const [fromDate, toDate] = formattedDate.split(',')
+      // if it's not a range, just skip
+      if (date.length !== 2) return
 
-          if ($el.hasClass('date-dropdown__input_from')) {
-            $el.val(fromDate)
-            $toDate.val(toDate)
+      // find apply button in datepicker
+      const $applyButton = instance.$datepicker.find(
+        '.datepicker--button[data-action="apply"]'
+      )
+      // if apply button was clicked
+      $applyButton.click(() => {
+        // in range, formattedDate is a string of 2 dates divided
+        // by separator specified in plugin settings
+        const separator = instance.opts.multipleDatesSeparator
+        // extract from & to dates from formattedDate
+        const [fromDate, toDate] = formattedDate.split(separator)
+        // set fromDate to actual input
+        instance.$el.val(fromDate)
+        // set toDate to fake input
+        $toDate.val(toDate)
+      })
 
-            if (!selected) {
-              $toDate.data('datepicker').selectDate(date)
-            }
-          } else if ($el.hasClass('date-dropdown__input_to')) {
-            $el.val(toDate)
-            $fromDate.val(fromDate)
+      // when clear button is pressed, both real and fake inputs have to be cleared.
+      // find clear button in current instance of datepicker
+      const $clearButton = instance.$datepicker.find(
+        '.datepicker--button[data-action="clear"]'
+      )
+      // if clear button is pressed
+      $clearButton.click(() => {
+        // clear fake input
+        $toDate.val('')
+      })
 
-            if (!selected) {
-              $fromDate.data('datepicker').selectDate(date)
-            }
-          }
-        }
-      }
-    })
-    .data('datepicker')
+      console.log(instance)
+    }
+  }
 
+  // the first input represents the datepicker
+  $fromDate.datepicker(datepickerSettings)
+  // the second input just refers to the first input
+  $toDate.click(() => $fromDate.focus())
+
+  // add apply button to datepicker
   const $buttons = $('.datepicker--buttons')
-  $buttons.append('<span class="datepicker--button" data-action="apply">Применить</span>')
+  $buttons.append(
+    '<span class="datepicker--button" data-action="apply">Применить</span>'
+  )
 })
