2588d3a4fd0e4c62f27fd1d7e93fa87896c20230
Artem <artmnglv@gmail.com>
2020-03-31 15:34:05 +0500

    implement login card

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -2,3 +2,4 @@ import './cards.scss'
 
 import '../../components/search-card/search-card'
 import '../../components/registration-card/registration-card'
+import '../../components/login-card/login-card'
