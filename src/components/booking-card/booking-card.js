aca7a0d322260e84278a2d42a0c49389f71efbc8
Artem <artmnglv@gmail.com>
2020-04-03 16:55:05 +0500

    Do some refactoring in booking card

--- src/components/booking-card/booking-card.js
+++ src/components/booking-card/booking-card.js
@@ -10,23 +10,18 @@ import DateDropdown from '../date-dropdown/date-dropdown'
 class BookingCard {
   constructor(component) {
     // jquerify raw dom element
-    this.$bookingCardComponent = $(component)
+    this.$component = $(component)
 
     // get the price
     this.price = this._data('price')
-    // find price element which will store the formatted price
-    this.$priceNumber = this.$bookingCardComponent.find(
-      '.booking-card__price-number'
-    )
-    // format price
-    this.formattedPrice = this._formatMoney(this.price)
+    // find price element which will store the formatted price and
     // put the formatted price there
-    this.$priceNumber.text(this.formattedPrice)
+    this.$priceNumber = this._findEl(this.$component, 'price-number').text(
+      this._formatMoney(this.price)
+    )
 
     // cache price details node
-    this.$priceDetails = this.$bookingCardComponent.find(
-      '.booking-card__price-details'
-    )
+    this.$priceDetails = this._findEl(this.$component, 'price-details')
 
     // compute the number of trip days.
     // get the arrival date and departure date
@@ -37,87 +32,60 @@ class BookingCard {
       new Date(this.departureDate),
       new Date(this.arrivalDate)
     )
+
     // put price and number of trip days in appropriate element
-    this.$pricePerDaysText = this.$priceDetails.find(
-      '.booking-card__price-per-days-text'
-    )
-    this.$pricePerDaysText.text(
-      `${this.formattedPrice} x ${this.tripDays} ${plural(
-        this.tripDays,
-        'сутки',
-        'суток'
-      )}`
-    )
-    // compute price for trip days
-    this.priceForTripDays = this.price * this.tripDays
+    this.$pricePerDaysText = this._findEl(
+      this.$priceDetails,
+      'price-per-days-text'
+    ).text(this.formattedPricePerDays)
+
     // put formatted price in appropriate element
-    this.$pricePerDaysTotalValue = this.$priceDetails.find(
-      '.booking-card__price-per-days-total-value'
+    this.$pricePerDaysTotalValue = this._findEl(
+      this.$priceDetails,
+      'price-per-days-total-value'
     )
-    this.$pricePerDaysTotalValue.text(this._formatMoney(this.priceForTripDays))
+    this.$pricePerDaysTotalValue.text(this._formatMoney(this.priceForTrip))
 
     // get discount, format it and put in appropriate element
     this.discount = this._data('discount')
-    this.$discount = this.$priceDetails.find('.booking-card__discount')
-    this.$discount.text(`скидка ${this._formatMoney(this.discount)}`)
+    this.$discount = this._findEl(this.$priceDetails, 'discount').text(
+      `скидка ${this._formatMoney(this.discount)}`
+    )
 
     // get services, format it and put in appropriate element
     this.services = this._data('services')
-    this.$servicesValue = this.$priceDetails.find(
-      '.booking-card__services-value'
-    )
-    this.$servicesValue.text(this._formatMoney(this.services))
+    this.$servicesValue = this._findEl(
+      this.$priceDetails,
+      'services-value'
+    ).text(this._formatMoney(this.services))
 
     // get additional services, format it and put in appropriate element
     this.additionalServices = this._data('additional-services')
-    this.$additionalServicesValue = this.$priceDetails.find(
-      '.booking-card__additional-services-value'
-    )
-    this.$additionalServicesValue.text(
-      this._formatMoney(this.additionalServices)
-    )
+    this.$additionalServicesValue = this._findEl(
+      this.$priceDetails,
+      'additional-services-value'
+    ).text(this._formatMoney(this.additionalServices))
 
     // compute total value and put it in appropriate element
-    this.total =
-      this.priceForTripDays +
-      this.services +
-      this.additionalServices -
-      this.discount
-    this.$totalValue = this.$priceDetails.find('.booking-card__total-value')
-    this.$totalValue.text(this._formatMoney(this.total))
+    this.$totalValue = this._findEl(this.$priceDetails, 'total-value').text(
+      this._formatMoney(this.total)
+    )
 
     // find date dropdown raw element and initialize date dropdown
-    this.dateDropdownElement = this.$bookingCardComponent.find(
-      '.booking-card__date-dropdown'
-    )[0]
-    this.dateDropdown = new DateDropdown(this.dateDropdownElement)
+    this.$dateDropdown = this._findEl(this.$component, 'date-dropdown')
+    this.dateDropdown = new DateDropdown(this.$dateDropdown[0])
 
-    // when date is changed, compute number of trip days and recompute values
+    // when date is changed, update some elements
     this.dateDropdown.onSelect((fromDate, toDate) => {
       // compute trip days
       this.tripDays = this._dateDifference(toDate, fromDate)
       // update price per days text
-      this.$pricePerDaysText.text(
-        `${this.formattedPrice} x ${this.tripDays} ${plural(
-          this.tripDays,
-          'сутки',
-          'суток'
-        )}`
-      )
-
-      // update price for trip days
-      this.priceForTripDays = this.price * this.tripDays
+      this.$pricePerDaysText.text(this.formattedPricePerDays)
+      // update price for trip
+      this.priceForTrip = this.tripDays
       // put updated formatted price in appropriate element
-      this.$pricePerDaysTotalValue.text(
-        this._formatMoney(this.priceForTripDays)
-      )
-
+      this.$pricePerDaysTotalValue.text(this._formatMoney(this.priceForTrip))
       // compute total value and put it in total-value element
-      this.total =
-        this.priceForTripDays +
-        this.services +
-        this.additionalServices -
-        this.discount
       this.$totalValue.text(this._formatMoney(this.total))
     })
   }
@@ -140,7 +108,37 @@ class BookingCard {
   }
 
   _data(name) {
-    return this.$bookingCardComponent.data(name)
+    return this.$component.data(name)
+  }
+
+  // find BEM element
+  _findEl($root, elementClass) {
+    return $root.find(`.booking-card__${elementClass}`)
+  }
+
+  get formattedPricePerDays() {
+    return `${this.formattedPrice} x ${this.tripDays} ${plural(
+      this.tripDays,
+      'сутки',
+      'суток'
+    )}`
+  }
+
+  get priceForTrip() {
+    return this.price * this.tripDays
+  }
+
+  set priceForTrip(days) {
+    this.price * days
+  }
+
+  get total() {
+    return (
+      this.priceForTrip +
+      this.services +
+      this.additionalServices -
+      this.discount
+    )
   }
 }
 
