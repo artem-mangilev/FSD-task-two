7f20a4f187e2b7816592b0efe773c92fa3805594
Artem <artmnglv@gmail.com>
2020-04-03 17:28:47 +0500

    create class for search card component and initialize it in cards

--- src/components/search-card/search-card.js
+++ src/components/search-card/search-card.js
@@ -3,3 +3,17 @@ import './search-card.scss'
 import '../date-dropdown/date-dropdown'
 import '../dropdown/dropdown'
 import '../button/button'
+import DateDropdown from '../date-dropdown/date-dropdown'
+
+class SearchCard {
+  constructor(component) {
+    // jquerify component
+    this.$component = $(component)
+    // find date dropdown
+    this.$dateDropdown = this.$component.find('.search-card__date-dropdown')
+    // initialize dropdown
+    new DateDropdown(this.$dateDropdown[0])
+  }
+}
+
+export default SearchCard
--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -7,7 +7,9 @@ import '../../components/registration-card/registration-card'
 import '../../components/login-card/login-card'
 import '../../components/booking-card/booking-card'
 import BookingCard from '../../components/booking-card/booking-card'
+import SearchCard from '../../components/search-card/search-card'
 
 $(document).ready(() => {
   $('.booking-card').each((i, card) => new BookingCard(card))
+  $('.search-card').each((i, card) => new SearchCard(card))
 })
