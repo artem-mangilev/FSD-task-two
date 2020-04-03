e037be8d315427cac7730a065b9c0944b7e4db9e
Artem <artmnglv@gmail.com>
2020-04-03 18:46:52 +0500

    extend datepicker to inlined variant

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -2,14 +2,17 @@ import '../../layout/base'
 import '../../scss/style.scss'
 import './cards.scss'
 
+import '../../components/date-dropdown/date-dropdown'
 import '../../components/search-card/search-card'
 import '../../components/registration-card/registration-card'
 import '../../components/login-card/login-card'
 import '../../components/booking-card/booking-card'
 import BookingCard from '../../components/booking-card/booking-card'
 import SearchCard from '../../components/search-card/search-card'
+import DateDropdown from '../../components/date-dropdown/date-dropdown'
 
 $(document).ready(() => {
   $('.booking-card').each((i, card) => new BookingCard(card))
   $('.search-card').each((i, card) => new SearchCard(card))
+  $('.cards__date-dropdown').each((i, dropdown) => new DateDropdown(dropdown))
 })
