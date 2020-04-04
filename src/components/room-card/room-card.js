17711b022fdbca0165224d884d04e61822baa2fe
Artem <artmnglv@gmail.com>
2020-04-04 18:25:14 +0500

    create room-card component

---
+++ src/components/room-card/room-card.js
@@ -0,0 +1,22 @@
+import './room-card.scss'
+
+import '../rating/rating'
+
+import 'lightslider'
+import 'lightslider/dist/css/lightslider.css'
+
+class RoomCard {
+  constructor(component) {
+    // jquerify component
+    this.$component = $(component)
+
+    // find component for slider & initialize slider
+    this.$slider = this.$component.find('.room-card__gallery')
+    this.$slider.lightSlider({
+      item: 1,
+      adaptiveHeight: true,
+    })
+  }
+}
+
+export default RoomCard
--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -7,12 +7,15 @@ import '../../components/search-card/search-card'
 import '../../components/registration-card/registration-card'
 import '../../components/login-card/login-card'
 import '../../components/booking-card/booking-card'
+import '../../components/room-card/room-card'
 import BookingCard from '../../components/booking-card/booking-card'
 import SearchCard from '../../components/search-card/search-card'
 import DateDropdown from '../../components/date-dropdown/date-dropdown'
+import RoomCard from '../../components/room-card/room-card'
 
 $(document).ready(() => {
   $('.booking-card').each((i, card) => new BookingCard(card))
   $('.search-card').each((i, card) => new SearchCard(card))
   $('.cards__date-dropdown').each((i, dropdown) => new DateDropdown(dropdown))
+  $('.room-card').each((i, card) => new RoomCard(card))
 })
