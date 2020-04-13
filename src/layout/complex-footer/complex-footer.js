dcf68442a7be482c0f8539e98d82e44c948f8492
Artem <artmnglv@gmail.com>
2020-04-13 20:06:10 +0500

    implement complex footer layout component

---
+++ src/layout/complex-footer/complex-footer.js
@@ -0,0 +1,7 @@
+import '@components/logo/logo'
+import '@components/text/text'
+import '@components/label/label'
+import '@components/link/link'
+import '@components/text-field/text-field'
+
+import './complex-footer.scss'
--- src/pages/headers-and-footers/headers-and-footers.js
+++ src/pages/headers-and-footers/headers-and-footers.js
@@ -2,3 +2,4 @@ import '@/main.scss'
 
 import '@layout/base/base'
 import '@layout/header/header'
+import '@layout/complex-footer/complex-footer'
