7a0c7cfd3f5fb1e68b2976e27fa77df1a43974e3
Artem <artmnglv@gmail.com>
2020-04-13 20:38:56 +0500

    add margins and shadows to layout components in headers and footers page

--- src/pages/headers-and-footers/headers-and-footers.js
+++ src/pages/headers-and-footers/headers-and-footers.js
@@ -1,6 +1,8 @@
-import '@/main.scss'
 
 import '@layout/base/base'
 import '@layout/header/header'
 import '@layout/complex-footer/complex-footer'
 import '@layout/simple-footer/simple-footer'
+
+import '@/main.scss'
+import './headers-and-footers.scss'
