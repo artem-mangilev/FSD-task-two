a2230ed16c01a5b7ee8397f5d97fc644395f91ad
Artem <artmnglv@gmail.com>
2020-04-15 13:24:59 +0500

    use social-buttons component in complex footer layout component

--- src/layout/complex-footer/complex-footer.js
+++ src/layout/complex-footer/complex-footer.js
@@ -3,5 +3,6 @@ import '@components/text/text'
 import '@components/label/label'
 import '@components/link/link'
 import '@components/text-field/text-field'
+import '@components/social-buttons/social-buttons'
 
 import './complex-footer.scss'
