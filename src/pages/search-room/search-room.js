56d98dae8786d1d8e506e5d100058ffeeda858e3
Artem <artmnglv@gmail.com>
2020-04-18 21:25:29 +0500

    start implementing search room page

---
+++ src/pages/search-room/search-room.js
@@ -0,0 +1,25 @@
+import '@layout/header/header'
+import '@layout/room-card/room-card'
+import RoomCard from '@layout/room-card/room-card'
+import '@layout/complex-footer/complex-footer'
+
+import '@components/date-dropdown/date-dropdown'
+import DateDropdown from '@components/date-dropdown/date-dropdown'
+import '@components/dropdown/dropdown'
+import '@components/range-slider/range-slider'
+import '@components/checkbox-list/checkbox-list'
+import '@components/pagination/pagination'
+import '@components/title/title'
+
+import './search-room.scss'
+import '@/main.scss'
+
+const $page = $('.js-search-room')
+
+const filterDateDropdown = $page.find('.js-search-room__filter-date-dropdown')[0]
+new DateDropdown(filterDateDropdown)
+
+const $roomCards = $page.find('.search-room__room-preview')
+$roomCards.each((i, roomCard) => new RoomCard(roomCard))
+
+
