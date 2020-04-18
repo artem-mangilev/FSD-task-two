d9860e0d25154d4fc2e5b627318e83804f52a84e
Artem <artmnglv@gmail.com>
2020-04-18 17:03:12 +0500

    fix loading date-dropdown's script in landing page

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -1,9 +1,13 @@
 import '@layout/app-base/app-base'
 import '@layout/header/header'
 import '@layout/search-card/search-card'
+import SearchCard from '@layout/search-card/search-card'
 import '@layout/complex-footer/complex-footer'
 
 import '@components/text/text'
 
 import './landing-page.scss'
 import '@/main.scss'
+
+const searchCard = $('.js-landing-page__search-card')[0]
+new SearchCard(searchCard)
