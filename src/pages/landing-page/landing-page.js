2ed46906476185beaceddc1402d083f0728a7f8f
Artem <artmnglv@gmail.com>
2020-04-18 15:11:07 +0500

    implement landing page

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -1,5 +1,9 @@
 import '@layout/app-base/app-base'
 import '@layout/header/header'
+import '@layout/search-card/search-card'
+import '@layout/complex-footer/complex-footer'
+
+import '@components/text/text'
 
 import './landing-page.scss'
 import '@/main.scss'
