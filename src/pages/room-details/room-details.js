0e4c06ff16b0c468aa7f497c5ad34a9020605e25
Artem <artmnglv@gmail.com>
2020-04-23 15:23:43 +0500

    implement room details page

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -3,13 +3,19 @@ import '@/main.scss'
 
 import '@layout/header/header'
 import '@layout/booking-card/booking-card'
+import BookingCard from '@layout/booking-card/booking-card'
 import '@layout/complex-footer/complex-footer'
 
 import '@components/features/features'
 import '@components/comment/comment'
 import '@components/bullet-list/bullet-list'
 import '@components/text/text'
+import '@components/subtitle/subtitle'
 import '@components/pie-chart/pie-chart'
 import PieChart from '@components/pie-chart/pie-chart'
 
 $('.js-room-details__pie-chart').each((i, element) => new PieChart(element))
+
+$('.js-room-details__booking-card').each(
+  (i, element) => new BookingCard(element)
+)
