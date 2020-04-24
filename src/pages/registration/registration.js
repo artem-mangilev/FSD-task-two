20a838caca2a088e3edcae12d546f01899f052a0
Artem <artmnglv@gmail.com>
2020-04-24 13:44:37 +0500

    implement registration page

---
+++ src/pages/registration/registration.js
@@ -0,0 +1,6 @@
+import '@layout/header/header'
+import '@layout/registration-card/registration-card'
+import '@layout/complex-footer/complex-footer'
+
+import './registration.scss'
+import '@/main.scss'
