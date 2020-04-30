b0964972d9cbfd4734ae27ace4fb06cb0454b986
Artem <artmnglv@gmail.com>
2020-04-30 14:29:31 +0500

    set appropriate public path for github pages in production  config

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,8 +1,16 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.config')
 
+const GH_PAGES_PUBLIC_PATH = '/FSD-task-two/'
+
 const buildWebpackConfig = merge(baseWebpackConfig, {
-  mode: 'production'
+  mode: 'production',
+
+  output: {
+    filename: '[name].js',
+    path: baseWebpackConfig.externals.paths.dist,
+    publicPath: GH_PAGES_PUBLIC_PATH,
+  },
 })
 
 module.exports = new Promise((resolve, reject) => {
